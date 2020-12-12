24e8b559f4c9440c1488a97f48729d9157f7005b
q <uab21373@cuoly.com>
2020-12-12 19:34:57 +0300

    v

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import telebot
 bot = telebot.TeleBot("1435505429:AAFdRhAAa8ETSnhfb8_ZIyzCRL54iW6mEp4")
 @bot.message_handler(content_types=("text"))
 def get_text_messages(message):
-    bot.send_message(message.from_user.id, message.json["text"])
-    if message.json == "Привет":
-        bot.send_message(message.from_user.id, "Привет")
-    print(message)
+    words = message.json["text"].split()
+    for word in words:
+        bot.send_message(message.from_user.id, word)
 bot.polling(none_stop=True, interval=0)
