88b60f3b959997261835ec0d6554266ebb409482
q <uab21373@cuoly.com>
2020-12-12 19:37:10 +0300

    v

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import telebot
 bot = telebot.TeleBot("1435505429:AAFdRhAAa8ETSnhfb8_ZIyzCRL54iW6mEp4")
 @bot.message_handler(content_types=("text"))
 def get_text_messages(message):
+    print (message)
     words = message.json["text"].split()
     for word in words:
         bot.send_message(message.from_user.id, word)
